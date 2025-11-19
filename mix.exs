c0b0bc64b59ae1c44662fe1affc0314320b6d0c8
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2025-11-19 11:02:25 +0000

    Add package metadata

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Demand.MixProject do
   use Mix.Project
 
+  @description """
+  A prompt library for Elixir. Port of [demand](https://github.com/jdx/demand).
+  """
+  @github "https://github.com/gmcabrita/demand"
   def project do
     [
       app: :demand,
+      source_url: @github,
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -15,6 +22,23 @@ defmodule Demand.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      logo: nil,
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Gonçalo Mendes Cabrita"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github}
     ]
   end
 
