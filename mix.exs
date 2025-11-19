916cb21209cc3634ca5e0a1cef6f531640eee567
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2025-11-19 11:34:47 +0000

    Bump version

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Demand.MixProject do
     [
       app: :demand,
       source_url: @github,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       description: @description,
